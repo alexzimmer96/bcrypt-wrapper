996f3582839b3b401049402586f242780c1cf8d1
Alexander Zimmermann <alexander.zimmermann96@gmail.com>
2019-06-29 23:04:29 +0200

    Finished first version
    * wrapper is working now
    * unit-test coverage is 100%

--- bcrypt_wrapper.go
+++ bcrypt_wrapper.go
@@ -1,7 +1,6 @@
 package bcrypt_wrapper
 
 import (
-	"crypto/rand"
 	"golang.org/x/crypto/bcrypt"
 	"time"
 )
@@ -19,10 +18,10 @@ var (
 )
 
 type BCryptWrapper struct {
-	cost int
+	Cost int
 }
 
-// Returns a new BCrypt-Wrapper. Set cost "NilCost" for running a benchmark and using the benchmark-outcome as cost.
+// Returns a new BCrypt-Wrapper. Set Cost "NilCost" for running a benchmark and using the benchmark-outcome as Cost.
 // The benchmark will then use 250ms as default value (meaning max amount of 4 passwords per second per thread).
 func NewBCryptWrapper(cost int) *BCryptWrapper {
 	if cost == NilCost {
@@ -34,18 +33,14 @@ func NewBCryptWrapper(cost int) *BCryptWrapper {
 		cost = defaultMaxCost
 	}
 	return &BCryptWrapper{
-		cost: cost,
+		Cost: cost,
 	}
 }
 
-// Run a benchmark to get a suitable cost for your application. The benchmark will take a few seconds.
-// It will return the last cost-value which is within your maxtime.
+// Run a benchmark to get a suitable Cost for your application. The benchmark will take a few seconds.
+// It will return the last Cost-value which is within your maxtime.
 func GetSuitableCost(maxtime int64) int {
-	pwBytes := make([]byte, 16)
-	if _, err := rand.Read(pwBytes); err != nil {
-		pwBytes = []byte("E&dWBjxaE*8V")
-	}
-
+	pwBytes := []byte("E&dWBjxaE*8V")
 	var executionTime int64 = 0
 	var cost = defaultMinCost
 
@@ -63,26 +58,26 @@ func GetSuitableCost(maxtime int64) int {
 }
 
 // Compares a password to a hashed password. Returns an bcrypt.ErrMismatchedHashAndPassword when password does not match
-// the hash. Returns a newly hashed password when the cost from the previous one used was lower then specified in this
+// the hash. Returns a newly hashed password when the Cost from the previous one used was lower then specified in this
 // wrapper.
-func (wrapper *BCryptWrapper) CompareHashAndPassword(hashedPassword, password []byte) (error, []byte) {
+func (wrapper *BCryptWrapper) CompareHashAndPassword(hashedPassword, password []byte) ([]byte, error) {
 	if err := bcrypt.CompareHashAndPassword(hashedPassword, password); err != nil {
 		// Password is incorrect or there's something wrong with the encryption-string
-		return err, NilHash
+		return NilHash, err
 	}
-	if passwordCost, err := bcrypt.Cost(hashedPassword); passwordCost < wrapper.cost && err != nil {
-		// Password does not use actual cost. Needs re-hashing
-		newHash, err := wrapper.GenerateFromPassword(password)
+	passwordCost, err := bcrypt.Cost(hashedPassword)
+	if passwordCost < wrapper.Cost && err == nil {
+		// Password does not use actual Cost. Needs re-hashing
+		hashed, err := wrapper.GenerateFromPassword(password)
 		if err == nil {
-			// Just returning the new hash when there was not error creating it
-			return nil, newHash
+			return hashed, nil
 		}
 	}
 	// Returning no error and no new hash
-	return nil, NilHash
+	return NilHash, nil
 }
 
-// Generates a hash value from a password. Uses the wrappers-cost as cost-value for bcrypt.
+// Generates a hash value from a password. Uses the wrappers-Cost as Cost-value for bcrypt.
 func (wrapper *BCryptWrapper) GenerateFromPassword(password []byte) ([]byte, error) {
-	return bcrypt.GenerateFromPassword(password, wrapper.cost)
+	return bcrypt.GenerateFromPassword(password, wrapper.Cost)
 }
--- bcrypt_wrapper_test.go
+++ bcrypt_wrapper_test.go
@@ -2,11 +2,92 @@ package bcrypt_wrapper
 
 import (
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"testing"
-	"time"
 )
 
-func TestGetSuitableComplexity(t *testing.T) {
-	cost := GetSuitableCost(int64(time.Millisecond * 250))
-	fmt.Printf("found suitable cost: %d\n", cost)
+const (
+	password      = "E&dWBjxaE*8V"
+	wrongPassword = "e&dWBjxaE*8V"
+	hashed        = "$2y$12$Su9Bpprp2BYnc3f12T.DW.gpQSUBoPu41RaXguWHXQmVE4c.Wvwoe"
+	wrongHash     = "$2y$12$Saf423uhp2398fh98epfh3q47q9ph24734q."
+	outdatedHash  = "$2y$11$KPR/tNhxP0RQcO7gSNjgJuXwu1jrwkfuEt2resN98faTtNnzq0DMa"
+)
+
+func TestNewBCryptWrapper(t *testing.T) {
+	t.Run("NilCost should lead to automatic cost", func(t *testing.T) {
+		wrapper := NewBCryptWrapper(NilCost)
+		if wrapper.Cost < defaultMinCost || wrapper.Cost > defaultMaxCost {
+			fmt.Printf("excepted a value between %d and %d as cost, got %d.\n",
+				defaultMinCost, defaultMaxCost, wrapper.Cost)
+			t.Fail()
+		}
+	})
+	t.Run("Cannot undercut minCost", func(t *testing.T) {
+		wrapper := NewBCryptWrapper(defaultMinCost - 1)
+		if wrapper.Cost != defaultMinCost {
+			fmt.Printf("excepted %d as cost, got %d.\n", defaultMinCost, wrapper.Cost)
+			t.Fail()
+		}
+	})
+	t.Run("Cannot overcome maxCost", func(t *testing.T) {
+		wrapper := NewBCryptWrapper(defaultMaxCost + 1)
+		if wrapper.Cost != defaultMaxCost {
+			fmt.Printf("excepted %d as cost, got %d.\n", defaultMaxCost, wrapper.Cost)
+			t.Fail()
+		}
+	})
+	t.Run("set value directly", func(t *testing.T) {
+		wrapper := NewBCryptWrapper(14)
+		if wrapper.Cost != 14 {
+			fmt.Printf("excepted %d as cost, got %d.\n", 14, wrapper.Cost)
+			t.Fail()
+		}
+	})
+}
+
+func TestBCryptWrapper_CompareHashAndPassword(t *testing.T) {
+	wrapper := NewBCryptWrapper(12)
+	t.Run("wrong hash should result in error", func(t *testing.T) {
+		newpass, err := wrapper.CompareHashAndPassword([]byte(wrongHash), []byte(password))
+		if len(newpass) != 0 || err == nil {
+			t.Fail()
+		}
+	})
+	t.Run("right password with outdated password should newly hash password", func(t *testing.T) {
+		newpass, err := wrapper.CompareHashAndPassword([]byte(outdatedHash), []byte(password))
+		if err != nil {
+			fmt.Printf("failing test due to error: %s.\n", err.Error())
+			t.Fail()
+		}
+		if len(newpass) == 0 {
+			fmt.Print("failing because no new password was generated.\n")
+			t.Fail()
+		}
+	})
+	t.Run("right password and matching bcrypt cost", func(t *testing.T) {
+		newpass, err := wrapper.CompareHashAndPassword([]byte(hashed), []byte(password))
+		if len(newpass) != 0 || err != nil {
+			t.Fail()
+		}
+	})
+	t.Run("wrong password", func(t *testing.T) {
+		newpass, err := wrapper.CompareHashAndPassword([]byte(hashed), []byte(wrongPassword))
+		if err != bcrypt.ErrMismatchedHashAndPassword {
+			fmt.Print("failing because \"bcrypt.ErrMismatchedHashAndPassword\" was excepted as error-object.\n")
+			t.Fail()
+		}
+		if len(newpass) > 0 {
+			fmt.Print("failing because no new password should have been generated.")
+			t.Fail()
+		}
+	})
+}
+
+func TestBCryptWrapper_GenerateFromPassword(t *testing.T) {
+	wrapper := NewBCryptWrapper(12)
+	result, err := wrapper.GenerateFromPassword([]byte(password))
+	if costFound, err2 := bcrypt.Cost(result); err != nil || err2 != nil || costFound != wrapper.Cost {
+		t.Fail()
+	}
 }
